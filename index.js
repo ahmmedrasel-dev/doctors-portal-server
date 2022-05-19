de60b591a9222452bca216aac782611b3d7fe345
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-05-20 00:57:51 +0300

    create speciality for doctor

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ async function run() {
 
     app.get('/services', async (req, res) => {
       const query = {};
-      const cursor = servicesCollection.find(query);
+      const cursor = servicesCollection.find(query).project({ name: 1 });
       const services = await cursor.toArray();
       res.send(services)
     })
